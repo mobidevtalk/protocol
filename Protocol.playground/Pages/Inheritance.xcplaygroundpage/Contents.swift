7fded96f7c96fdfb018e0cfad53e7540dbce57bc
Shad Mazumder <shadmazumder@gmail.com>
2019-03-25 17:47:53 +0600

    Inheritance.

--- Protocol.playground/Pages/Inheritance.xcplaygroundpage/Contents.swift
+++ Protocol.playground/Pages/Inheritance.xcplaygroundpage/Contents.swift
@@ -1,1 +1,46 @@
 //: [Index](Index)
+
+import Foundation
+
+class SuperHero{
+    let name: String
+    let originalName: String
+    let capeColor: String
+    
+    init(name: String, originalName: String, capeColor: String) {
+        self.name = name
+        self.originalName = originalName
+        self.capeColor = capeColor
+    }
+    
+    func speciality() -> String{
+        return ""
+    }
+    
+    var profile: String?{
+        return "Known as: \(name) \n Original Name: \(originalName) \n Wears a \(capeColor) colored Cape.\n Speciality is: \n\(speciality())"
+    }
+}
+
+class Batman: SuperHero{
+    init() {
+        super.init(name: "Batman", originalName: "Bruce Wayne", capeColor: "Black")
+    }
+    
+    override func speciality() -> String {
+        return "Very techie and extremely cool 😎"
+    }
+}
+
+class WonderWoman: SuperHero{
+    init() {
+        super.init(name: "Wonder Woman", originalName: "Diana", capeColor: "Red")
+    }
+    
+    override func speciality() -> String {
+        return "Come on, she is a Demigoddess"
+    }
+}
+
+Batman().profile
+WonderWoman().profile
