ac8af3bab1352d97c4e39d705fc7e3e56490bedb
Shad Mazumder <shadmazumder@gmail.com>
2019-04-04 20:02:06 +0600

    Setting up the team.

--- Protocol.playground/Pages/Avengers.xcplaygroundpage/Contents.swift
+++ Protocol.playground/Pages/Avengers.xcplaygroundpage/Contents.swift
@@ -1,1 +1,27 @@
 //: [Index](Index)
+
+import Foundation
+
+/*:
+ A case study of Avengers
+ */
+
+protocol Avenger{
+    var name: String {get}
+}
+
+struct IronMan: Avenger{
+    var name = "Iron Man"
+}
+
+struct CaptainAmerica: Avenger{
+    var name = "Captain America"
+}
+
+struct Hulk: Avenger{
+    var name = "Hulk"
+}
+
+struct Thor: Avenger{
+    var name = "Thor"
+}
