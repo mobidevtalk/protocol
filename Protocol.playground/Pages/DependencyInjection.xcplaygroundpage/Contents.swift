fd797e97586b3e9ed02fd6a02d9f7287388efbda
Shad Mazumder <shadmazumder@gmail.com>
2019-07-06 17:35:06 +0600

    Implementing two different class among them we will use the dependency Injection.

--- Protocol.playground/Pages/DependencyInjection.xcplaygroundpage/Contents.swift
+++ Protocol.playground/Pages/DependencyInjection.xcplaygroundpage/Contents.swift
@@ -10,28 +10,14 @@ hero.superPower = "N/A"
 SuperHero(superPower: "Speed")
 
 
+//: Protocol usage on Dependency Injection
 
-enum Scale: CustomStringConvertible{
-    case noob
-    case basic
-    case intermediate
-    case advance
-    case superAdvance
-    
-    var description: String{
-        switch self {
-        case .noob:
-            return "Below basic"
-        case .basic:
-            return "Basic"
-        case .intermediate:
-            return "Skillfull"
-        case .advance:
-            return "Very skilfull"
-        case .superAdvance:
-            return "Super Skillfull"
-        }
-    }
+enum Scale: String{
+    case noob = "Below basic"
+    case basic = "Basic"
+    case intermediate = "Skillfull"
+    case advance = "Very skilfull"
+    case superAdvance = "Super Skillfull"
 }
 
 enum Capability: CustomStringConvertible{
@@ -42,38 +28,70 @@ enum Capability: CustomStringConvertible{
     var description: String{
         switch self {
         case .streanth(let scale):
-            return "\(scale) level of Streanth)"
+            return "\(scale.rawValue) level of Streanth"
         case .speed(let scale):
-            return "\(scale) level of Speed"
+            return "\(scale.rawValue) level of Speed"
         case .fly(let scale):
-            return "\(scale) level of Flying"
+            return "\(scale.rawValue) level of Flying"
         }
     }
 }
 
-
-protocol Avenger{
-    var name: String {get}
-    var capability: [Capability] {get}
-    func resume() -> String
+protocol Recruiter{
+    func submitInfo(name: String, superHeroName: String, capabilities: [Capability])
+    func formattedInfo() -> String?
 }
 
-protocol Broadcast{
-    var info: String {get}
-}
-
-struct IronMan: Avenger{
-    private var broadcast: Broadcast
+struct IronMan{
+    private let name = "Tony Stark"
+    private let aka = "Iron Man"
+    private let capabilities: [Capability] = [.streanth(.advance), .fly(.superAdvance)]
     
-    var name = "Iron Man"
-    var capability: [Capability]{
-        return [.streanth(.advance), .fly(.superAdvance)]
+    let recruiter: Recruiter
+    
+    func submitResume(){
+        recruiter.submitInfo(name: name, superHeroName: aka, capabilities: capabilities)
     }
     
-    func resume() -> String {
-        let capabilityString = capability.reduce(into: "Capabilities:", { $0 = "\n" + $1.description })
-        return "Name: \(name)\n\(capabilityString)"
+    func resume(){
+        print(recruiter.formattedInfo() ?? "")
     }
 }
 
+class Agent: Recruiter{
+    private var resume: String?
+    
+    func submitInfo(name: String, superHeroName: String, capabilities: [Capability]) {
+        let capabilityString = capabilities.reduce(into: "Capabilities:", { $0 = "\n" + $1.description })
+        resume = "Name: \(name)\nAKA: \(superHeroName)\n\(capabilityString)"
+    }
+    
+    func formattedInfo() -> String? {
+        return resume
+    }
+}
 
+
+class Agency: Recruiter{
+    private var resume: String?
+    
+    func submitInfo(name: String, superHeroName: String, capabilities: [Capability]) {
+        let splitedName = name.split(separator: " ")
+
+        let fullName = "First name: " + (splitedName.first ?? "") + (splitedName.count > 1 ? "\(splitedName.dropFirst(1).reduce(into: "\nLast name", { $0 += " " + $1 }))" : "")
+        
+        let capabilityString = capabilities.reduce(into: "\nCapabilities:", { $0 += "\n" + $1.description })
+        
+        resume = "Commonly Know as: \(superHeroName)\n\(fullName)\n\(capabilityString)"
+    }
+    
+    func formattedInfo() -> String? {
+        return resume
+    }
+}
+
+let agency = Agency()
+
+let ironMan = IronMan(recruiter : agency)
+ironMan.submitResume()
+ironMan.resume()
